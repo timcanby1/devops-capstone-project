3ef6d723b3658a7332ade964a9a5c0d136911a8b
timcanby1 <kangyinglee0501@outlook.com>
2024-02-14 14:08:40 -0500

    Fix flake8 linting errors

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -27,6 +27,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 class TestAccountService(TestCase):
     """Account Service Tests"""
+    
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
